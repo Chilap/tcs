b9f5a68d851e135679e2d1f837613a21db523f34
Chilap <cheng.chilap.221@gmail.com>
2021-04-23 01:50:07 +0800

    clubhose

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,65 +10,62 @@ export default function Home() {
       </Head>
 
       <div className="bg-primary min-h-screen flex flex-col items-center py-0.5 ">
-      {/* TODO: sticky title */}
-      <h1 className="text-5xl text-gray font-bold m-8 sticky top-0 pt-8 pb-2 py-4 bg-primary w-screen text-center">
-        🔗 Links
-      </h1>
-      {/* <h1 className="text-5xl text-white font-bold m-8 sticky top-0 bg-gray-800 w-72 text-center">🔗 Links</h1> */}
+        {/* TODO: sticky title */}
+        <h1 className="text-5xl text-gray font-bold m-8 sticky top-0 pt-8 pb-2 py-4 bg-primary w-screen text-center">
+          🔗 Links
+        </h1>
+        {/* <h1 className="text-5xl text-white font-bold m-8 sticky top-0 bg-gray-800 w-72 text-center">🔗 Links</h1> */}
 
-      <div className="grid grid-cols-1">
-        <a
-          href="./"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-        >
-          🏠 The Clubhouse Show
-        </a>
-        <a
-          href="https://google.com"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-          target="_blank"
-        >
-          📝 Blogs
-        </a>
-
-        
-        <a
-          href="https://LinkedIn.com"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-          target="_blank"
-        >
-         🤝 LinkedIn
-        </a>
-        <a
-          href="https://instagram.com/"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-          target="_blank"
-        >
-          📷 Instagram
-        </a>
-        <a
-          href="https://telegram.com"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-          target="_blank"
-        >
-          📞 Telegram
-        </a>
-        <a
-          href="https://twitter.com"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-          target="_blank"
-        >
-          🦅 Twitter
-        </a>
-        <a
-          href="https://substack.com/for-writers"
-          className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
-          target="_blank"
-        >
-          📧 Join our Newsletter
-        </a>
+        <div className="grid grid-cols-1">
+          <a
+            href="./"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+          >
+            ☀️ Home Page
+          </a>
+          <a
+            href="https://www.joinclubhouse.com/club/%E5%BB%A3%E6%9D%B1%E8%A9%B1%E7%AF%80%E7%9B%AE%E5%B7%A1%E7%A6%AE-cantonese-show"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+          >
+            🏠 The Clubhouse Show
+          </a>
+          <a
+            href="https://LinkedIn.com"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+            target="_blank"
+          >
+            🤝 LinkedIn
+          </a>
+          <a
+            href="https://instagram.com/"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+            target="_blank"
+          >
+            📷 Instagram
+          </a>
+          <a
+            href="https://telegram.com"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+            target="_blank"
+          >
+            📞 Telegram
+          </a>
+          <a
+            href="https://twitter.com"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+            target="_blank"
+          >
+            🦅 Twitter
+          </a>
+          <a
+            href="https://substack.com/for-writers"
+            className="bg-white rounded-lg py-4 pl-8 w-72 my-2 shadow hover:shadow-xl duration-200"
+            target="_blank"
+          >
+            📧 Join our Newsletter
+          </a>
+        </div>
       </div>
-    </div>
 
       {/* <footer className={styles.footer}>
         <a
@@ -80,5 +77,5 @@ export default function Home() {
         </a>
       </footer> */}
     </div>
-  )
+  );
 }
